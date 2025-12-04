e1e9a3366dfb3eb1dcc4e0ebdb4c76576cf8c814
Volker Leukhardt <volker.leukhardt@gmail.com>
2025-12-04 14:19:32 +0000

    Debug push

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -92,6 +92,7 @@ pub fn init_routes(cfg: &mut web::ServiceConfig) {
             .service(league::delete_poll)
             .service(league::send_team_chat)
             .service(league::get_team_chat)
+            .service(league::mark_team_chat_read)  // Must come before edit/delete to avoid UUID parsing conflict
             .service(league::edit_team_chat)
             .service(league::delete_team_chat)
     );
